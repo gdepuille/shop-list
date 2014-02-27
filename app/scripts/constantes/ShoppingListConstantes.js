4d3cf6c47c38568fb28237ff681e1085ac3960de
nartawak <nartawak@gmail.com>
2014-02-27 13:45:28 +0100

    save

---
+++ app/scripts/constantes/ShoppingListConstantes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('shopListApp').constant('ShoppingListConstantes', {
+    states : {
+
+    },
+    events : {
+
+    },
+    firebase : {
+        ROOT : 'https://listecourses.firebaseio.com/'
+    }
+
+});
