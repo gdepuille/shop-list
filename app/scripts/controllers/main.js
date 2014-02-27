92964a60d66adf63a5fc735f94f02ebf4174c0cb
Gregory DEPUILLE <gregory.depuille@gmail.com>
2014-02-27 22:37:07 +0100

    Ajout du leader avec le nom de l'application et le nom du compte connecté.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('shoppingListApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.user = {
+            firstName: "God",
+            lastName: "Michel",
+            username: "god.michel@ste.fr"
+        };
+    }
+);
