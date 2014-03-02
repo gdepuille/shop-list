80a5b02ce888a90291a71368861ee5960c0c5e5a
Gregory DEPUILLE <gregory.depuille@gmail.com>
2014-03-02 20:41:37 +0100

    Look un peu plus classe de la page principale avec les implémentations de login.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('shopListApp')
+angular.module('shoppingListApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
