bdd8c6edc29d479b12e8dafd8e7681d3d197175c
Gregory DEPUILLE <gregory.depuille@gmail.com>
2014-03-03 23:34:11 +0100

    Finalisation du layout

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,73 @@
 angular.module('shoppingListApp')
     .controller('MainCtrl', function ($scope) {
         $scope.user = {
+            id: '12345',
             firstName: "God",
             lastName: "Michel",
-            username: "god.michel@ste.fr"
+            username: "god.michel@ste.fr",
+            provider: 'twitter',
+            premium: false,
+            dateCreation: '2014-03-03T22:34:00Z',
+            lastConnection : '2014-03-03T22:42:00Z',
+            lists: [12, 43, 45]
+        };
+
+        $scope.lists = [{
+            id: 1,
+            name: 'Achat pour Haloween',
+            ownerId: 12345,
+            icon: 'base64 : wxccddsfsbb==',
+            items: [{
+                id: 34566,
+                name: 'Beurre',
+                icon: 'base64 : dgdfhfhdfgdhdghdghd==',
+                qte: 1,
+                checked: false
+            }, {
+                id: 123,
+                name: 'Carambar',
+                icon: 'base64 : dgdfhfhdfgdhdghdghd==',
+                qte: 3,
+                checked: false
+            }]
+        }, {
+            id: 2,
+            name: 'Achat pour Noël',
+            ownerId: 12345,
+            icon: 'base64 : wxccddsfsbb==',
+            items: [{
+                id: 12,
+                name: 'Foie gras',
+                icon: 'base64 : dgdfhfhdfgdhdghdghd==',
+                qte: 1,
+                checked: false
+            }, {
+                id: 345,
+                name: 'Champagne',
+                icon: 'base64 : dgdfhfhdfgdhdghdghd==',
+                qte: 3,
+                checked: false
+            }]
+        }];
+
+        $scope.selectedList = {
+            id: 2,
+            name: 'Achat pour Noël',
+            ownerId: 12345,
+            icon: 'base64 : wxccddsfsbb==',
+            items: [{
+                id: 12,
+                name: 'Foie gras',
+                icon: 'base64 : dgdfhfhdfgdhdghdghd==',
+                qte: 1,
+                checked: false
+            }, {
+                id: 345,
+                name: 'Champagne',
+                icon: 'base64 : dgdfhfhdfgdhdghdghd==',
+                qte: 3,
+                checked: false
+            }]
         };
     }
 );
